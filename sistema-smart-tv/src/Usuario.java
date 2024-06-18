61da85cf6795dffe76462d69eaa43074796a32c9
Sandro2s <sandros.silva@gmail.com>
2024-06-18 19:38:28 -0300

    treinando o que  foi aprendido até aula metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Default : " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Default : " + smartTv.canal);
+
+
+        System.out.println("VolumeAtual: " + smartTv.volume);
+
+        System.out.println("TV Ligada? : "   + smartTv.ligada);
+        System.out.println("Canal Default : " + smartTv.canal);
+        System.out.println("Volume Default: "+ smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status --> TV Ligada?: "+ smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status --> TV Ligada?: "+ smartTv.ligada);
+    }
+}
