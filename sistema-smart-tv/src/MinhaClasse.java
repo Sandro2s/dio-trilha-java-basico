cc5c91524d01bc54232dfe3d50997a9857b78e20
Sandro2s <sandros.silva@gmail.com>
2024-06-21 11:18:56 -0300

    tarefa-20240621_1118-Teste Compilação

--- sistema-smart-tv/src/MinhaClasse.java
+++ sistema-smart-tv/src/MinhaClasse.java
@@ -1,6 +1,7 @@
-public class MinhaClasse {
+public class minhaClasse {
     
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo terminal");
+        System.out.println("Oi novamente! fui executado pelo terminal");
     }
+    
 }
--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,7 +1,7 @@
 public class SmartTv {
     boolean ligada=false;
     int canal=1;
-    int volume= 25;
+    int volume= 28;
 
     public void mudarCanal(int novoCanal){
         canal = novoCanal;
