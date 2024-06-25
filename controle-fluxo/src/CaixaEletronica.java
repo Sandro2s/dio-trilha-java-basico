30c4969a34f73eed578f200db746df23ed4301a6
Sandro2s <sandros.silva@gmail.com>
2024-06-25 18:13:10 -0300

    tarefa_20240625_1812 - Inclusão da clausula Else

---
+++ controle-fluxo/src/CaixaEletronica.java
@@ -0,0 +1,14 @@
+public class CaixaEletronica {
+    public static void main(String[] args) throws Exception {
+       double saldo = 25.0;
+       double valorSolicitado = 30.0;
+
+       if(valorSolicitado < saldo)
+       saldo = saldo - valorSolicitado;
+       else
+       System.out.println("Saldo Insuficiente");
+
+       System.out.println(saldo);
+
+    }
+}
