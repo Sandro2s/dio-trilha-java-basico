a08c1a154daf5331aa0d24be91521becb2dcd6e1
Sandro2s <sandros.silva@gmail.com>
2024-06-25 18:49:23 -0300

    tarefa_20240625_1848-Melhorias no controle de fluxo.

--- controle-fluxo/src/ResultadoEscola.java
+++ controle-fluxo/src/ResultadoEscola.java
@@ -1,10 +1,19 @@
 public class ResultadoEscola {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
 
-        if(nota >= 7)
-        System.out.println("Aprovado");
-        else
-        System.out.println("Reprovado");
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }
+         else if (nota >= 5 && nota < 7){
+            System.out.println("Recuperação");
+         }  else{
+            System.out.println("Reprovado");
+         }  
+            
+        
+            
+        
+        
     }
 }
