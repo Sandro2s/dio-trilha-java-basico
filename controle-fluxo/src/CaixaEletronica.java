554e5c38bdec0ca7ad3bdeb658b22cd45299e635
Sandro2s <sandros.silva@gmail.com>
2024-06-25 18:26:04 -0300

    tarefa_20240625_1825 - Atualização do if/else

--- controle-fluxo/src/CaixaEletronica.java
+++ controle-fluxo/src/CaixaEletronica.java
@@ -1,15 +1,14 @@
 public class CaixaEletronica {
     public static void main(String[] args) throws Exception {
        double saldo = 25.0;
-       double valorSolicitado = 30.0;
+       double valorSolicitado = 25.1;
 
-       if(valorSolicitado < saldo)
-       saldo = saldo - valorSolicitado;
-       else
-       System.out.println("Saldo Insuficiente");
+       if(valorSolicitado <= saldo){
+        saldo = saldo - valorSolicitado;
+        System.out.println("Novo saldo: R$" + saldo);
+       }else
+       System.out.println("Saldo Insuficiente : R$" + saldo);
 
-       
-       System.out.println(saldo);
 
     }
 }
