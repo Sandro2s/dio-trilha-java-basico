141b848ee97d7c2fbdd7923f26eeaa447ef932b6
Sandro Soares Silva <37371977+Sandro2s@users.noreply.github.com>
2024-07-18 18:55:08 -0300

    Add files via upload

---
+++ controle-fluxo/src/ExemploForArray01.java
@@ -0,0 +1,12 @@
+public class ExemploForArray01{
+
+public static void main(String[] args) {
+    // em arrays o indice inicia em Zero
+     String alunos[] = {"felipe","jonas","julia","marcos"};
+
+     for(int x=0; x<alunos.length;x++){
+    System.out.println("O aluno no indice x = " + x +" é " + alunos[x]);
+     }
+    
+ }
+}
---
+++ controle-fluxo/src/ExemploForArray02.java
@@ -0,0 +1,10 @@
+public class ExemploForArray02 {
+    public static void main(String[] args) {
+        String alunos[] = {"felipe","jonas","julia","marcos"};
+        
+        //For Each forma de consulta apreviada
+        for(String aluno : alunos){
+            System.out.println("O nome do aluno é " + aluno);
+        }
+    }
+}
