d9f4aa5ad3f887d9d7a28e97ed9e39fae875b041
Sandro Soares <sandros.silva@gmail.com>
2024-08-02 19:55:19 -0300

    tarefa_20240802_1952 - Codigo Final Funcionando

--- DesafioControleFluxo/src/contador.java
+++ DesafioControleFluxo/src/contador.java
@@ -12,17 +12,20 @@ public class contador {
 				int parametroDois = terminal.nextInt();
 				terminal.close();
 
-				if(parametroUm >= parametroDois){
-					throw new ParametrosInvalidosException();
-
-				}
-				System.out.println("######## Incio de impressãao ############################");
-				for (int i = parametroUm; i < parametroDois; i++){
-					System.out.println(i);
-				}
+				contar(parametroUm,parametroDois);
+				
 			} catch (ParametrosInvalidosException e){
 				System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
 				
 			} 
-		}			
+		}	
+		static void contar(int parametroUm,int parametroDois) throws ParametrosInvalidosException{
+			if(parametroUm >= parametroDois){
+				throw new ParametrosInvalidosException();
+			}    
+			    //For para impressão com base nos parametros.
+				for (int i = parametroUm; i < parametroDois; i++){
+					System.out.println(i);
+		        }
+	    }		
 	}
